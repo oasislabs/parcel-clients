8810779674690fa4d49feb9b7835c5c8bc4e2ccf
Charlie Jacobson <charlie.m.jacobson@gmail.com>
2021-02-22 11:47:59 -0800

    [Docs] [Data Access] simplify data access tutorial, remove grants section
    Apply 5 suggestion(s) to 1 file(s)
    
    rebase fixup

--- examples/data-access/src/main.ts
+++ examples/data-access/src/main.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 // #region snippet-config
 const acmeId = '0d9f279b-a5d8-7260-e090-ff1a7659ba3b' as IdentityId;
-const tokenSourceAcme = {
+const tokenSource = {
   principal: acmeId,
   privateKey: {
     kty: 'EC',
@@ -16,47 +16,55 @@ const tokenSourceAcme = {
     d: '9ssmJBm_mDIKpxdB2He-zIMeclYtDGQcBv2glEH7r5k',
   },
 } as const;
-const bobId = '6cc5defa-af04-512f-6aa3-c13f64d03a8b' as IdentityId;
-const tokenSourceBob = {
-  principal: bobId,
-  privateKey: {
-    kty: 'EC',
-    alg: 'ES256',
-    use: 'sig',
-    crv: 'P-256',
-    kid: 'EA8OhUgQBJ4GNO7hwhr3JiG6L-oKqPqYA_2ZctSBtAw',
-    x: 'L3u92SCDDcsbLpFI2NUmwWtu4Xit26y2lUt8w3Da6g8',
-    y: '857gZPxQiiO3mRSKuK8-42_QG65Q1HVzU__h8n7hLeQ',
-    d: 'gXQmoXWOQvh8X4fM0d9b5aVYro3jhCkx0svuez9yMhk',
-  },
-} as const;
 // #endregion snippet-config
 
 // #region snippet-identity-acme-connect
 // Connect to ACME's identity.
-const parcelAcme = new Parcel(tokenSourceAcme, {
+const parcel = new Parcel(tokenSource, {
   apiUrl: process.env.API_URL,
 });
-await parcelAcme.getCurrentIdentity();
 // #endregion snippet-identity-acme-connect
 
-// #region snippet-upload
-// Upload a dataset and assign ownership to Bob.
+// By default, datasets are owned by the uploading identity
+// #region snippet-upload-default-owner
+const acmeIdentity = await parcel.getCurrentIdentity();
+console.log(`Uploading data with identity: ${acmeIdentity.id})`);
+
 const data = 'The weather will be sunny tomorrow and cloudy on Tuesday.';
 const datasetDetails = { title: 'Weather forecast summary' };
-console.log(`Uploading data for Bob (owner ID: ${bobId})`);
-const dataset = await parcelAcme.uploadDataset(data, {
+const acmeDataset = await parcel.uploadDataset(data, {
+  details: datasetDetails,
+}).finished;
+console.log(`Created dataset ${acmeDataset.id} with owner ${acmeDataset.owner}`);
+// #endregion snippet-upload-default-owner
+
+// Dataset owners can always download their data
+// #region snippet-download-owner
+console.log(`Downloading dataset ${acmeDataset.id} with identity ${acmeIdentity.id}`);
+let download = parcel.downloadDataset(acmeDataset.id);
+let saver = fs.createWriteStream(`./acme_dataset`);
+await download.pipeTo(saver);
+console.log(`Dataset ${acmeDataset.id} downloaded to ./acme_dataset`);
+
+const acmeData = fs.readFileSync('./acme_dataset', 'utf-8');
+console.log(`Here's the data: ${acmeData}`);
+// #endregion snippet-download-owner
+
+// Upload a dataset and assign ownership to a sample end user (e.g. "Bob")
+// #region snippet-upload-user-data
+const bobId = '3cc86186-7093-e71e-c5f1-e95d6d07a3cf'; // REPLACE ME
+console.log(`Uploading data for end user Bob (ID: ${bobId})`);
+const bobDataset = await parcel.uploadDataset(data, {
   details: datasetDetails,
   owner: bobId,
 }).finished;
+console.log(`Created dataset ${bobDataset.id} with owner ${bobDataset.owner}`);
+// #endregion snippet-upload-user-data
 
-console.log(`Created dataset ${dataset.id} with owner ${dataset.owner}`);
-// #endregion snippet-upload
-
+// By default, we do not have permission to access data owned by other users
 // #region snippet-download-acme-error
-// We can't download data, if we are not granted to do so. Let's try to download it anyway.
-let download = parcelAcme.downloadDataset(dataset.id);
-let saver = fs.createWriteStream(`./bob_data_by_acme`);
+download = parcel.downloadDataset(bobDataset.id);
+saver = fs.createWriteStream(`./bob_data_by_acme`);
 try {
   console.log(`Attempting to access Bob's dataset without permission...`);
   await download.pipeTo(saver);
@@ -67,88 +75,23 @@ try {
 
 console.log();
 
+/**
+ * At this point, we need Bob to grant us permission to use his data.
+ * Specifically, we need to:
+ *  - Redirect Bob to steward.oasislabs.com/apps/:id/join
+ *  - Have Bob grant us permission
+ */
+
+// Now, accessing the dataset succeeds
 // #region snippet-download-bob-success
-// Create Bob's parcel instance using his API access token.
-const parcelBob = new Parcel(tokenSourceBob, {
-  apiUrl: process.env.API_URL,
-});
-await parcelBob.getCurrentIdentity();
-
-// Now let's try to download it again, this time as Bob.
-console.log(`Attempting to access Bob's dataset by Bob himself...`);
-download = parcelBob.downloadDataset(dataset.id);
-saver = fs.createWriteStream(`./bob_data_by_bob`);
+console.log(`Attempting to access Bob's dataset with ACME identity ${acmeIdentity.id}`);
+download = parcel.downloadDataset(bobDataset.id);
+saver = fs.createWriteStream(`./bob_data_by_acme`);
 await download.pipeTo(saver);
-console.log(`Dataset ${dataset.id} has been downloaded to ./bob_data_by_bob`);
+console.log(`Dataset ${bobDataset.id} has been downloaded to ./bob_data_by_acme`);
 
-const secretDataByBob = fs.readFileSync('./bob_data_by_bob', 'utf-8');
-console.log(`Here's the data: ${secretDataByBob}`);
+const bobData = fs.readFileSync('./bob_data_by_acme', 'utf-8');
+console.log(`Here's the data: ${bobData}`);
 // #endregion snippet-download-bob-success
 
 console.log();
-
-// #region snippet-create-grant
-// Grant ACME access to Bob's data.
-const grant = await parcelBob.createGrant({
-  grantee: acmeId,
-  conditions: { 'dataset.id': { $eq: dataset.id } },
-});
-console.log(
-  `New grant ${grant.id} for dataset ${dataset.id} and grantee ${acmeId} has been created`,
-);
-// #endregion snippet-create-grant
-
-// #region snippet-download-acme-success
-// ACME is now allowed to download Bob's data.
-console.log(`Attempting to access Bob's dataset with granted permission...`);
-download = parcelAcme.downloadDataset(dataset.id);
-saver = fs.createWriteStream(`./bob_data_by_acme`);
-await download.pipeTo(saver);
-console.log(`Dataset ${dataset.id} has been downloaded to ./bob_data_by_acme`);
-
-const secretDataByAcme = fs.readFileSync('./bob_data_by_acme', 'utf-8');
-console.log(`Here's the data: ${secretDataByAcme}`);
-// #endregion snippet-download-acme-success
-
-console.log();
-
-// #region snippet-upload-with-tags
-// Upload a dataset with a specific tag and assign ownership to Bob.
-const dataWithTags = 'Vreme bo jutri sončno, v torek pa oblačno.';
-const datasetDetailsWithTags = {
-  title: 'Povzetek vremenske napovedi',
-  tags: ['lang_sl'],
-};
-console.log(`Uploading data for Bob with ${datasetDetailsWithTags.tags} tags (owner ID: ${bobId})`);
-const datasetWithTags = await parcelAcme.uploadDataset(dataWithTags, {
-  details: datasetDetailsWithTags,
-  owner: bobId,
-}).finished;
-
-console.log(
-  `Created dataset ${datasetWithTags.id} with owner ${datasetWithTags.owner} and tags ${datasetWithTags.details.tags}`,
-);
-// #endregion snippet-upload-with-tags
-
-// #region snippet-create-grant-with-tags
-// Grant ACME access to any Bob's dataset containing tag 'lang:sl'.
-const grantWithTags = await parcelBob.createGrant({
-  grantee: acmeId,
-  conditions: { 'dataset.details.tags': { $any: { $eq: 'lang_sl' } } },
-});
-console.log(
-  `New grant ${grantWithTags.id} for datasets with tags 'lang_sl' and grantee ${acmeId} has been created`,
-);
-// #endregion snippet-create-grant-with-tags
-
-// #region snippet-download-acme-success-with-tags
-// ACME is now allowed to download Bob's data.
-console.log(`Attempting to access Bob's dataset with tags-based grant...`);
-download = parcelAcme.downloadDataset(datasetWithTags.id);
-saver = fs.createWriteStream(`./bob_data_by_acme_with_tags`);
-await download.pipeTo(saver);
-console.log(`Dataset ${datasetWithTags.id} has been downloaded to ./bob_data_by_acme_with_tags`);
-
-const secretDataByAcmeWithTags = fs.readFileSync('./bob_data_by_acme_with_tags', 'utf-8');
-console.log(`Here's the data: ${secretDataByAcmeWithTags}`);
-// #endregion snippet-download-acme-success-with-tags
