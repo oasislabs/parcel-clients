4f523691d75087cc17075485b4b49a4b98d2b85a
lukaw3d <lukajeran@oasislabs.com>
2021-06-03 17:47:36 +0200

    Extract jest integration helpers

--- test/jest/integration/large-upload-download.spec.ts
+++ test/jest/integration/large-upload-download.spec.ts
@@ -1,44 +1,8 @@
 import fs from 'fs';
 
 import crypto from 'crypto';
-import { JWK } from 'node-jose';
-
-import Parcel, { PrivateJWK, PublicJWK, Document } from '@oasislabs/parcel';
-
-const apiUrl = process.env.PARCEL_API_URL ?? 'http://localhost:4242/v1';
-
-export async function generateJWKPair() {
-  const keyPair = await JWK.createKey('EC', 'P-256', {
-    alg: 'ES256',
-    use: 'sig',
-  });
-  const publicKey = keyPair.toJSON(false) as PublicJWK;
-  const privateKey = keyPair.toJSON(true) as PrivateJWK;
-  return { publicKey, privateKey };
-}
-
-/** Creates a Parcel client using a bootstrap identity. */
-async function bootstrapParcel() {
-  const parcel = new Parcel('not required in dev mode when identity creation is unauthenticated', {
-    apiUrl,
-  });
-
-  const { privateKey, publicKey } = await generateJWKPair();
-  const credential = {
-    principal: `bootstrap${Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)}`,
-    privateKey,
-  };
-  await parcel.createIdentity({
-    tokenVerifiers: [
-      {
-        sub: credential.principal,
-        iss: credential.principal,
-        publicKey,
-      },
-    ],
-  });
-  return new Parcel(credential, { apiUrl });
-}
+import type { Parcel, Document } from '@oasislabs/parcel';
+import { bootstrapParcel } from './helpers';
 
 async function createLargeFile() {
   await fs.promises.writeFile('/tmp/large_input', '');
@@ -51,17 +15,17 @@ describe('Large file (100MiB)', () => {
   let parcel: Parcel;
   let doc: Document;
 
-  beforeAll(async () => {
+  it('bootstrap', async () => {
     await createLargeFile();
     parcel = await bootstrapParcel();
   });
 
-  it('Upload', async () => {
+  it('upload', async () => {
     const readStream = fs.createReadStream('/tmp/large_input');
     doc = await parcel.uploadDocument(readStream, null).finished;
   }, 30_000);
 
-  it('Download', async () => {
+  it('download', async () => {
     const writeStream = fs.createWriteStream('/tmp/large_output');
     const download = doc.download();
     await download.pipeTo(writeStream);
