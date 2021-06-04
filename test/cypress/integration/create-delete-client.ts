6b350a01eb49d54c5d18633de7ebb2b7fe85f718
lukaw3d <lukajeran@oasislabs.com>
2021-06-05 00:24:20 +0200

    Add cypress integration test to create and delete a client

---
+++ test/cypress/integration/create-delete-client.ts
@@ -0,0 +1,52 @@
+/// <reference types="../fixtures/types" />
+// eslint-disable-next-line import/extensions
+import { App, Client, ClientType, Parcel } from '../../..';
+import { bootstrapParcel } from './helpers';
+
+describe('Client', () => {
+  let parcel: Parcel;
+  let app: App;
+  let client: Client;
+
+  it('bootstrap', async () => {
+    parcel = await bootstrapParcel();
+  });
+
+  it('create', async () => {
+    app = await parcel.createApp({
+      admins: [],
+      allowUserUploads: false,
+      collaborators: [],
+      homepageUrl: 'https://oasislabs.com',
+      identity: {
+        tokenVerifiers: [],
+      },
+      inviteOnly: false,
+      invites: [],
+      logoUrl: 'https://oasislabs.com',
+      name: 'a',
+      organization: '',
+      privacyPolicy: 'https://oasislabs.com',
+      published: true,
+      shortDescription: '',
+      termsAndConditions: 'https://oasislabs.com',
+      acceptanceText: '',
+      brandingColor: '',
+      category: '',
+      extendedDescription: '',
+      invitationText: '',
+      rejectionText: '',
+    });
+    client = await parcel.createClient(app.id, {
+      type: ClientType.Frontend,
+      name: 'a',
+      redirectUris: ['https://oasislabs.com'],
+      postLogoutRedirectUris: ['https://oasislabs.com'],
+    });
+  });
+
+  it('delete', async () => {
+    await client.delete();
+    await app.delete();
+  });
+});
