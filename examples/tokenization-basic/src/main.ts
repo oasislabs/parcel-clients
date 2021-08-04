3d3d1b4776f9b797ec90a569798677e6fb3fdb5e
Mitja T <mitjat@oasislabs.com>
2021-08-04 12:26:32 +0200

    Change tokenization example code from recipe to art

--- examples/tokenization-basic/src/main.ts
+++ examples/tokenization-basic/src/main.ts
@@ -18,28 +18,40 @@ const parcelBob = new Parcel({
 
 // --- Create a new data-backed token held by Bob.
 // #region snippet-mint-token
-console.log('Creating a new data-backed token owned by Bob.');
-const recipeToken = await parcelBob.mintToken({
-  name: "Bob's World Famous Chicken & Pepper Recipe (keto!)",
+console.log('Creating a new data-backed token owned by Bob, the artist.');
+const artToken = await parcelBob.mintToken({
+  name: "Bob's Saguaro Sunset painting",
   grant: {
     condition: null, // Allow full access to anyone holding the token.
   },
   supply: 1, // It's an NFT.
 });
-console.log('The recipe token is:', recipeToken);
+console.log('The art token is:', artToken);
 // #endregion snippet-mint-token
 
 // --- Upload and tokenize a data asset provided by Bob.
 // #region snippet-tokenize-asset
 console.log('Create a new document and prepare it for tokenization.');
-const recipeText = '14g butter; 15g chicken sausage; 18g feta; 20g green pepper; 1.5min baking';
-const recipeDocument = await parcelBob.uploadDocument(recipeText, {
+const artContents = String.raw`
+  ........::::::::::::..           .......|...............::::::::........
+     .:::::;;;;;;;;;;;:::::.... .     \   | ../....::::;;;;:::::.......
+         .       ...........   / \\_   \  |  /     ......  .     ........./\
+...:::../\\_  ......     ..._/'   \\\_  \###/   /\_    .../ \_.......   _//
+.::::./   \\\ _   .../\    /'      \\\\#######//   \/\   //   \_   ....////
+    _/      \\\\   _/ \\\ /  x       \\\\###////      \////     \__  _/////
+  ./   x       \\\/     \/ x X           \//////                   \/////
+ /     XxX     \\/         XxX X                                    ////   x
+-----XxX-------------|-------XxX-----------*--------|---*-----|------------X--
+       X        _X      *    X      **         **             x   **    *  X
+      _X                    _X           x                *          x     X_
+`;
+const artDocument = await parcelBob.uploadDocument(artContents, {
   owner: 'escrow', // ⚠️  The data must be owned by the escrow identity to be tokenized. This can be done after uploading, too.
   toApp: undefined,
 }).finished;
 
 console.log('Add the document to the token.');
-await recipeToken.addAsset(recipeDocument.id);
+await artToken.addAsset(artDocument.id);
 // More data assets can also be added (by anyone).
 // #endregion snippet-tokenize-asset
 
@@ -60,19 +72,21 @@ const parcelAcme = new Parcel({
 const acmeIdentity = await parcelAcme.getCurrentIdentity();
 
 // #region snippet-transfer-token
+// Bob transfers the token.
 const transferReceipt = await parcelBob.transferToken(
-  recipeToken.id,
+  artToken.id,
   1, // Transfer one token, the entire supply.
   acmeIdentity.id,
 );
-console.log('Receipt of token transfer to Acme:', transferReceipt);
+console.log('Receipt of token transfer to Acme, the buyer:', transferReceipt);
 
-const recipeChunks = [];
-const recipeDownload = parcelAcme.downloadDocument(recipeDocument.id);
-for await (const chunk of recipeDownload) {
-  recipeChunks.push(chunk);
+// Acme can now download the underlying data.
+const artChunks = [];
+const artDownload = parcelAcme.downloadDocument(artDocument.id);
+for await (const chunk of artDownload) {
+  artChunks.push(chunk);
 }
 
-console.log('Acme now has access to the recipe!');
-assert.strictEqual(Buffer.concat(recipeChunks).toString(), recipeText);
+console.log('Acme now has access to the art! (And Bob doesn\'t anymore.)');
+assert.strictEqual(Buffer.concat(artChunks).toString(), artContents);
 // #endregion snippet-transfer-token
