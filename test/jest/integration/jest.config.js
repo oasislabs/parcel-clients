bf99b7916695ceb60eb5f09b3b6ce2424d3d5ea0
lukaw3d <lukajeran@oasislabs.com>
2021-09-04 05:32:23 +0200

    Fix test:integration:jest Segmentation Fault by not using bundle.cjs

--- test/jest/integration/jest.config.js
+++ test/jest/integration/jest.config.js
@@ -9,6 +9,12 @@ const config = {
     // Override moduleNameMapper to remove `@oasislabs/parcel => src/index`
     // mappings, otherwise importing '../../..' imports `lib/index.d.ts` and
     // causes duplicate conflicting ky Response declaration.
+
+    // Override using bundle.cjs because jest uses package.json "main".
+    // Jest + bundle.cjs causes Segmentation Fault when downloading.
+    // TODO: https://gitlab.com/oasislabs/parcel/-/issues/740
+    '^\\.\\./\\.\\./\\.\\.$': '<rootDir>/lib/index',
+  },
 };
 
 export default config;
